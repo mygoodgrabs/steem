d65c3e6acd565eebd5e26a537749d4c341912318
Bartek Wrona <wrona@syncad.com>
2017-09-06 17:38:59 +0200

    Issue #1268 rename of STEEMIT_ constants to STEEM_
    All constants were renamed as requested with once exception - STEEMIT_SYMBOL was renamed to STEEM_SYMBOL_NAME due to conflict to existing constant: STEEM_SYMBOL

--- libraries/chain/smt_evaluator.cpp
+++ libraries/chain/smt_evaluator.cpp
@@ -12,32 +12,32 @@ namespace steemit { namespace chain {
 
 void smt_setup_evaluator::do_apply( const smt_setup_operation& o )
 {
-   FC_ASSERT( _db.has_hardfork( STEEMIT_SMT_HARDFORK ), "SMT functionality not enabled until hardfork ${hf}", ("hf", STEEMIT_SMT_HARDFORK) );
+   FC_ASSERT( _db.has_hardfork( STEEM_SMT_HARDFORK ), "SMT functionality not enabled until hardfork ${hf}", ("hf", STEEM_SMT_HARDFORK) );
 }
 
 void smt_cap_reveal_evaluator::do_apply( const smt_cap_reveal_operation& o )
 {
-   FC_ASSERT( _db.has_hardfork( STEEMIT_SMT_HARDFORK ), "SMT functionality not enabled until hardfork ${hf}", ("hf", STEEMIT_SMT_HARDFORK) );
+   FC_ASSERT( _db.has_hardfork( STEEM_SMT_HARDFORK ), "SMT functionality not enabled until hardfork ${hf}", ("hf", STEEM_SMT_HARDFORK) );
 }
 
 void smt_refund_evaluator::do_apply( const smt_refund_operation& o )
 {
-   FC_ASSERT( _db.has_hardfork( STEEMIT_SMT_HARDFORK ), "SMT functionality not enabled until hardfork ${hf}", ("hf", STEEMIT_SMT_HARDFORK) );
+   FC_ASSERT( _db.has_hardfork( STEEM_SMT_HARDFORK ), "SMT functionality not enabled until hardfork ${hf}", ("hf", STEEM_SMT_HARDFORK) );
 }
 
 void smt_setup_inflation_evaluator::do_apply( const smt_setup_inflation_operation& o )
 {
-   FC_ASSERT( _db.has_hardfork( STEEMIT_SMT_HARDFORK ), "SMT functionality not enabled until hardfork ${hf}", ("hf", STEEMIT_SMT_HARDFORK) );
+   FC_ASSERT( _db.has_hardfork( STEEM_SMT_HARDFORK ), "SMT functionality not enabled until hardfork ${hf}", ("hf", STEEM_SMT_HARDFORK) );
 }
 
 void smt_set_setup_parameters_evaluator::do_apply( const smt_set_setup_parameters_operation& o )
 {
-   FC_ASSERT( _db.has_hardfork( STEEMIT_SMT_HARDFORK ), "SMT functionality not enabled until hardfork ${hf}", ("hf", STEEMIT_SMT_HARDFORK) );
+   FC_ASSERT( _db.has_hardfork( STEEM_SMT_HARDFORK ), "SMT functionality not enabled until hardfork ${hf}", ("hf", STEEM_SMT_HARDFORK) );
 }
 
 void smt_set_runtime_parameters_evaluator::do_apply( const smt_set_runtime_parameters_operation& o )
 {
-   FC_ASSERT( _db.has_hardfork( STEEMIT_SMT_HARDFORK ), "SMT functionality not enabled until hardfork ${hf}", ("hf", STEEMIT_SMT_HARDFORK) );
+   FC_ASSERT( _db.has_hardfork( STEEM_SMT_HARDFORK ), "SMT functionality not enabled until hardfork ${hf}", ("hf", STEEM_SMT_HARDFORK) );
 }
 
 } }
